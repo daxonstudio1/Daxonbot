888af639db7c9b3b120318187ced0054c6a52b0e
daxonstudio1 <daxonstudio1@gmail.com>
2026-03-21 10:22:14 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+# ===== CONFIG =====
+GUILD_ID = 1235640804148645928
+VERIFIED_ROLE = "Verified"
+MEMBER_ROLE = "Member"
+
+# ===== INTENTS =====
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# =========================================================
+# READY
+# =========================================================
+@bot.event
+async def on_ready():
+    print(f"✅ Bot online jako {bot.user}")
+
+# =========================================================
+# START (RAILWAY)
+# =========================================================
+bot.run(os.getenv("TOKEN"))
